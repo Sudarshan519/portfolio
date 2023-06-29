4ab3e3f5d2bd957148a42f989166a2d8d226776c
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-29 08:10:38 +0545

    update environment

---
+++ apps/attendance_system/route_base.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter
+
+
+router =APIRouter(include_in_schema=True)
+
+from apps.attendance_system.schemas.company import CompanyBase
+from apps.attendance_system.schemas.employee import EmployeeBase
+from apps.attendance_system.schemas.attendance import AttendanceModel,AttendanceTodayDetailModel
+
+@router.get('index',tags=[])
+async def hello():
+    return {"message":"Hello from attendance api"}
+
+@router.post('add_company')
+async def add_company(company:CompanyBase):
+    return {"message":"Company added successfully."}
+
+@router.post('add_employee')
+async def add_employee(employee:EmployeeBase):
+    return {"message":"Company added successfully."}
+
+@router.post('add_attendance',response_model=AttendanceModel)
+async def add_attendance():
+    return {'message':"Addendance added successfully."}
+@router.post('logout',response_model=AttendanceModel)
+async def add_logout():
+    
+    return {'message':"Attendance updated"}
+
+@router.post('start_break',response_model=AttendanceModel)
+async def start_break():
+    return {'message':"Attendance updated"}
+
+@router.post('stop_break',response_model=AttendanceModel)
+async def stop_break():
+    return {'message':"Attendance updated"}
+
+@router.post('get-today-details',response_model=AttendanceTodayDetailModel)
+async def get_today():
+    return {}
