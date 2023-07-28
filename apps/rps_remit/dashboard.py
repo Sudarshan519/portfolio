65ebdf0d239d9e09199f3f032e6e80e26d66a78c
sudarshan519 <sudarshanshrestha519@gmail.com>
2023-07-29 01:25:50 +0545

    removed

--- apps/rps_remit/dashboard.py
+++ apps/rps_remit/dashboard.py
@@ -1,25 +1,25 @@
-from ast import List
-from fastapi import APIRouter,Depends
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
+# from ast import List
+# from fastapi import APIRouter,Depends
+# from pydantic import BaseModel
+# from sqlalchemy.orm import Session
 
-from db.models.user import Users
-from db.session import get_db
+# from db.models.user import Users
+# from db.session import get_db
 
 
-router=APIRouter(tags=['Dashboard'])
+# router=APIRouter(tags=['Dashboard'])
 
-@router.get("/")
-def index():
-   return {"message": "Hello World from remit dashboard app"}
-class UserSchema(BaseModel):
-   id:int
-   email:str
-   verified:bool
-   is_active:bool
+# @router.get("/")
+# def index():
+#    return {"message": "Hello World from remit dashboard app"}
+# class UserSchema(BaseModel):
+#    id:int
+#    email:str
+#    verified:bool
+#    is_active:bool
 
-   class Config:
-      orm_mode=True
-@router.get('/users',response_model=list[UserSchema])
-def all_users(db:Session=Depends(get_db)):
-   return db.query(Users).filter(Users.is_superuser==False).all()
+#    class Config:
+#       orm_mode=True
+# @router.get('/users',response_model=list[UserSchema])
+# def all_users(db:Session=Depends(get_db)):
+#    return db.query(Users).filter(Users.is_superuser==False).all()
